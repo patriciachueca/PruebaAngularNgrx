9218c51c7bcc8228bb776857026f806f8241aef9
patriciachueca <patrychueca@gmail.com>
2020-12-29 11:15:33 +0100

    ADD - Add imports to App.Module

--- ngrx-shopping-list/src/app/app.module.ts
+++ ngrx-shopping-list/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { ShoppingReducer } from './store/reducers/shopping.reducer';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      shopping: ShoppingReducer,
+    }),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
