5402d2d9e51972f5beff6d59476c196fd7bbfe55
patriciachueca <patrychueca@gmail.com>
2020-12-29 12:42:42 +0100

    UPDATE - Add New Item

--- ngrx-shopping-list/src/app/app.component.ts
+++ ngrx-shopping-list/src/app/app.component.ts
@@ -3,14 +3,16 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { ShoppingItem } from './store/models/shopping-item.model';
 import { AppState } from './store/models/app-state.model';
-
+import { AddItemAction } from './store/actions/shopping.actions';
+import { v4 as uuid } from 'uuid';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   shoppingItems: Observable<Array<ShoppingItem>>;
+  newShoppingItem: ShoppingItem = { id: '', name: '' }
 
   constructor(private store: Store<AppState>) { }
 
@@ -18,5 +20,13 @@ export class AppComponent implements OnInit{
     this.shoppingItems = this.store.select(store => store.shopping);
     //También podríamos haber seleccionado nuestro shoppin
     //this.shoppingItems = this.store.select('shopping');
+  }
+
+  addItem() {  //Dispatch un action en la store 
+    this.newShoppingItem.id = uuid();
+
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+
+    this.newShoppingItem = { id: '', name: '' };
   }
 }
